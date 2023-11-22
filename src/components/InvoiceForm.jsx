81c3aeb877e7c77ef33cf9149e6749710bafe743
Shivam Garg <shivamgarg1324@gmail.com>
2023-11-22 18:37:00 +0530

    done

--- src/components/InvoiceForm.jsx
+++ src/components/InvoiceForm.jsx
@@ -15,6 +15,7 @@ import { addInvoice, updateInvoice } from "../redux/invoicesSlice";
 import { Link, useParams, useLocation, useNavigate } from "react-router-dom";
 import generateRandomId from "../utils/generateRandomId";
 import { useInvoiceListData } from "../redux/hooks";
+import { BiTrash } from "react-icons/bi";
 
 const InvoiceForm = () => {
   const dispatch = useDispatch();
@@ -141,10 +142,10 @@ const InvoiceForm = () => {
     handleCalculateTotal();
   };
 
-  // const editField = (name, value) => {
-  //   setFormData({ ...formData, [name]: value });
-  //   handleCalculateTotal();
-  // };
+  const editField = (name, value) => {
+    setFormData({ ...formData, [name]: value });
+    handleCalculateTotal();
+  };
 
   const onCurrencyChange = (selectedOption) => {
     setFormData({ ...formData, currency: selectedOption.currency });
@@ -187,17 +188,28 @@ const InvoiceForm = () => {
     }
   };
   
-  const editField = (name, value, id) => {
-    if (isEditBulk) {
-      FormData.map((item) => {
-        console.log(item)
-      });
-      
-    } 
-  
-    // handleCalculateTotal();
+
+  const handleItemChange = (index, field, value) => {    
+    const updatedFormData = JSON.parse(JSON.stringify(formData));
+    updatedFormData[index].items[0][field] = value;
+    setFormData(updatedFormData);
   };
-  
+
+  const handleEditbulkInvoice = () => {
+    formData.forEach((data) => {
+      dispatch(updateInvoice({ id: data.id, updatedInvoice: data }));
+    });
+    navigate('/');
+  };
+  const alternatingRowClass = {
+    backgroundColor: '#0000', 
+  };
+
+  const handleItemDelete = (dataIndex, itemIndex) => {
+    const updatedFormData = JSON.parse(JSON.stringify(formData));
+    updatedFormData[dataIndex].items.splice(itemIndex,1);
+    setFormData(updatedFormData);
+  };
 
   return (
     <div>
@@ -212,47 +224,88 @@ const InvoiceForm = () => {
     {isEditBulk ? 
      (
       <div>
-          <Table striped bordered hover>
+          <Table bordered hover>
             <thead>
               <tr>
-                <th>ID</th>
-                <th>Current Date</th>
-                <th>Invoice Number</th>
-                <th>Item ID</th>
-                <th>Item Name</th>
-                <th>Item Description</th>
-                <th>Item Price</th>
-                <th>Item Quantity</th>
+                <th className="col-1 text-center">ID</th>
+                <th className="text-center">Current Date</th>
+                <th className="text-center">Invoice Number</th>
+                <th className="text-center">Item ID</th>
+                <th className="text-center">Item Name</th>
+                <th className="text-center">Item Description</th>
+                <th className="text-center">Item Price</th>
+                <th className="text-center">Item Quantity</th>
+                <th className="text-center">Item Delete</th>
               </tr>
             </thead>
             <tbody>
               {formData.map((data, index) => (
                 <React.Fragment key={`data-${index}`}>
-                  <tr>
-                    <td>{data.id}</td>
-                    <td>{data.currentDate}</td>
-                    <td>{data.invoiceNumber}</td>
-                    <td>{data.items[0].itemId}</td>
-                    <td>{data.items[0].itemName}</td>
-                    <td>{data.items[0].itemDescription}</td>
-                    <td>{data.items[0].itemPrice}</td>
-                    <td>{data.items[0].itemQuantity}</td>
-                  </tr>
-                  {data.items.slice(1).map((item, itemIndex) => (
-                    <tr key={`item-${itemIndex}`}>
-                      <td colSpan="4"></td>
-                      <td>{item.itemId}</td>
-                      <td>{item.itemName}</td>
-                      <td>{item.itemDescription}</td>
-                      <td>{item.itemPrice}</td>
-                      <td>{item.itemQuantity}</td>
+                  {data.items.map((item, itemIndex) => (
+                    <tr key={`item-${index}-${itemIndex}`} >
+                      <td className="text-center">
+                        {itemIndex === 0 ? data.id : null}
+                      </td>
+                      <td>{data.currentDate}</td>
+                      <td className="text-center">{data.invoiceNumber}</td>
+                      <td className="text-center">{item.itemId}</td>
+                      <td >
+                        <input
+                          type="text"
+                          value={item.itemName}
+                          onChange={(e) =>
+                            handleItemChange(index, 'itemName', e.target.value)
+                          }
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="text"
+                          value={item.itemDescription}
+                          onChange={(e) =>
+                            handleItemChange(index, 'itemDescription', e.target.value)
+                          }
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="number"
+                          value={item.itemPrice}
+                          onChange={(e) =>
+                            handleItemChange(index, 'itemPrice', e.target.value)
+                          }
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="number"
+                          value={item.itemQuantity}
+                          onChange={(e) =>
+                            handleItemChange(index, 'itemQuantity', e.target.value)
+                          }
+                        />
+                      </td>
+                      <td>
+                      <Button variant="danger" onClick={() => handleItemDelete(index, itemIndex)}>
+                        <div className="d-flex align-items-center justify-content-center gap-2">
+                          <BiTrash />
+                        </div>
+                      </Button>
+                      </td>
                     </tr>
                   ))}
                 </React.Fragment>
               ))}
             </tbody>
           </Table>
-      
+
+            <Button
+              variant="dark"
+              className="d-block w-30 mb-2 mx-auto"
+              onClick={handleEditbulkInvoice}
+            >
+              Save Invoice
+            </Button>
       </div>
       )
       :
@@ -550,285 +603,3 @@ const InvoiceForm = () => {
 
 export default InvoiceForm;
 
-
-// {formData.map((formData)=>{
-//   return(
-//   <Form onSubmit={openModal}>
-// <Row>
-// <Col md={8} lg={9}>
-//   <Card className="p-4 p-xl-5 my-3 my-xl-4">
-//     <div className="d-flex flex-row align-items-start justify-content-between mb-3">
-//       <div className="d-flex flex-column">
-//         <div className="d-flex flex-column">
-//           <div className="mb-2">
-//             <span className="fw-bold">Current&nbsp;Date:&nbsp;</span>
-//             <span className="current-date">{formData.currentDate}</span>
-//           </div>
-//         </div>
-//         <div className="d-flex flex-row align-items-center">
-//           <span className="fw-bold d-block me-2">Due&nbsp;Date:</span>
-//           <Form.Control
-//             type="date"
-//             value={formData.dateOfIssue}
-//             name="dateOfIssue"
-//             onChange={(e) => editField(e.target.name, e.target.value,formData.infod)}
-//             style={{ maxWidth: "150px" }}
-//             required
-//           />
-//         </div>
-//       </div>
-//       <div className="d-flex flex-row align-items-center">
-//         <span className="fw-bold me-2">Invoice&nbsp;Number:&nbsp;</span>
-//         <Form.Control
-//           type="number"
-//           value={formData.invoiceNumber}
-//           name="invoiceNumber"
-//           onChange={(e) => editField(e.target.name, e.target.value)}
-//           min="1"
-//           style={{ maxWidth: "70px" }}
-//           required
-//         />
-//       </div>
-//     </div>
-//     <hr className="my-4" />
-//     <Row className="mb-5">
-//       <Col>
-//         <Form.Label className="fw-bold">Bill to:</Form.Label>
-//         <Form.Control
-//           placeholder="Who is this invoice to?"
-//           rows={3}
-//           value={formData.billTo}
-//           type="text"
-//           name="billTo"
-//           className="my-2"
-//           onChange={(e) => editField(e.target.name, e.target.value)}
-//           autoComplete="name"
-//           required
-//         />
-//         <Form.Control
-//           placeholder="Email address"
-//           value={formData.billToEmail}
-//           type="email"
-//           name="billToEmail"
-//           className="my-2"
-//           onChange={(e) => editField(e.target.name, e.target.value)}
-//           autoComplete="email"
-//           required
-//         />
-//         <Form.Control
-//           placeholder="Billing address"
-//           value={formData.billToAddress}
-//           type="text"
-//           name="billToAddress"
-//           className="my-2"
-//           autoComplete="address"
-//           onChange={(e) => editField(e.target.name, e.target.value)}
-//           required
-//         />
-//       </Col>
-//       <Col>
-//         <Form.Label className="fw-bold">Bill from:</Form.Label>
-//         <Form.Control
-//           placeholder="Who is this invoice from?"
-//           rows={3}
-//           value={formData.billFrom}
-//           type="text"
-//           name="billFrom"
-//           className="my-2"
-//           onChange={(e) => editField(e.target.name, e.target.value)}
-//           autoComplete="name"
-//           required
-//         />
-//         <Form.Control
-//           placeholder="Email address"
-//           value={formData.billFromEmail}
-//           type="email"
-//           name="billFromEmail"
-//           className="my-2"
-//           onChange={(e) => editField(e.target.name, e.target.value)}
-//           autoComplete="email"
-//           required
-//         />
-//         <Form.Control
-//           placeholder="Billing address"
-//           value={formData.billFromAddress}
-//           type="text"
-//           name="billFromAddress"
-//           className="my-2"
-//           autoComplete="address"
-//           onChange={(e) => editField(e.target.name, e.target.value)}
-//           required
-//         />
-//       </Col>
-//     </Row>
-//     <InvoiceItem
-//       onItemizedItemEdit={onItemizedItemEdit}
-//       onRowAdd={handleAddEvent}
-//       onRowDel={handleRowDel}
-//       currency={formData.currency}
-//       items={formData.items}
-//     />
-//     <Row className="mt-4 justify-content-end">
-//       <Col lg={6}>
-//         <div className="d-flex flex-row align-items-start justify-content-between">
-//           <span className="fw-bold">Subtotal:</span>
-//           <span>
-//             {formData.currency}
-//             {formData.subTotal}
-//           </span>
-//         </div>
-//         <div className="d-flex flex-row align-items-start justify-content-between mt-2">
-//           <span className="fw-bold">Discount:</span>
-//           <span>
-//             <span className="small">
-//               ({formData.discountRate || 0}%)
-//             </span>
-//             {formData.currency}
-//             {formData.discountAmount || 0}
-//           </span>
-//         </div>
-//         <div className="d-flex flex-row align-items-start justify-content-between mt-2">
-//           <span className="fw-bold">Tax:</span>
-//           <span>
-//             <span className="small">({formData.taxRate || 0}%)</span>
-//             {formData.currency}
-//             {formData.taxAmount || 0}
-//           </span>
-//         </div>
-//         <hr />
-//         <div
-//           className="d-flex flex-row align-items-start justify-content-between"
-//           style={{ fontSize: "1.125rem" }}
-//         >
-//           <span className="fw-bold">Total:</span>
-//           <span className="fw-bold">
-//             {formData.currency}
-//             {formData.total || 0}
-//           </span>
-//         </div>
-//       </Col>
-//     </Row>
-//     <hr className="my-4" />
-//     <Form.Label className="fw-bold">Notes:</Form.Label>
-//     <Form.Control
-//       placeholder="Thanks for your business!"
-//       name="notes"
-//       value={formData.notes}
-//       onChange={(e) => editField(e.target.name, e.target.value)}
-//       as="textarea"
-//       className="my-2"
-//       rows={1}
-//     />
-//   </Card>
-// </Col>
-// <Col md={4} lg={3}>
-//   <div className="sticky-top pt-md-3 pt-xl-4">
-    
-//     <InvoiceModal
-//       showModal={isOpen}
-//       closeModal={closeModal}
-//       info={{
-//         isOpen,
-//         id: formData.id,
-//         currency: formData.currency,
-//         currentDate: formData.currentDate,
-//         invoiceNumber: formData.invoiceNumber,
-//         dateOfIssue: formData.dateOfIssue,
-//         billTo: formData.billTo,
-//         billToEmail: formData.billToEmail,
-//         billToAddress: formData.billToAddress,
-//         billFrom: formData.billFrom,
-//         billFromEmail: formData.billFromEmail,
-//         billFromAddress: formData.billFromAddress,
-//         notes: formData.notes,
-//         total: formData.total,
-//         subTotal: formData.subTotal,
-//         taxRate: formData.taxRate,
-//         taxAmount: formData.taxAmount,
-//         discountRate: formData.discountRate,
-//         discountAmount: formData.discountAmount,
-//       }}
-//       items={formData.items}
-//       currency={formData.currency}
-//       subTotal={formData.subTotal}
-//       taxAmount={formData.taxAmount}
-//       discountAmount={formData.discountAmount}
-//       total={formData.total}
-//     />
-//     <Form.Group className="mb-3">
-//       <Form.Label className="fw-bold">Currency:</Form.Label>
-//       <Form.Select
-//         onChange={(event) =>
-//           onCurrencyChange({ currency: event.target.value })
-//         }
-//         className="btn btn-light my-1"
-//         aria-label="Change Currency"
-//       >
-//         <option value="$">USD (United States Dollar)</option>
-//         <option value="£">GBP (British Pound Sterling)</option>
-//         <option value="¥">JPY (Japanese Yen)</option>
-//         <option value="$">CAD (Canadian Dollar)</option>
-//         <option value="$">AUD (Australian Dollar)</option>
-//         <option value="$">SGD (Singapore Dollar)</option>
-//         <option value="¥">CNY (Chinese Renminbi)</option>
-//         <option value="₿">BTC (Bitcoin)</option>
-//       </Form.Select>
-//     </Form.Group>
-//     <Form.Group className="my-3">
-//       <Form.Label className="fw-bold">Tax rate:</Form.Label>
-//       <InputGroup className="my-1 flex-nowrap">
-//         <Form.Control
-//           name="taxRate"
-//           type="number"
-//           value={formData.taxRate}
-//           onChange={(e) => editField(e.target.name, e.target.value)}
-//           className="bg-white border"
-//           placeholder="0.0"
-//           min="0.00"
-//           step="0.01"
-//           max="100.00"
-//         />
-//         <InputGroup.Text className="bg-light fw-bold text-secondary small">
-//           %
-//         </InputGroup.Text>
-//       </InputGroup>
-//     </Form.Group>
-//     <Form.Group className="my-3">
-//       <Form.Label className="fw-bold">Discount rate:</Form.Label>
-//       <InputGroup className="my-1 flex-nowrap">
-//         <Form.Control
-//           name="discountRate"
-//           type="number"
-//           value={formData.discountRate}
-//           onChange={(e) => editField(e.target.name, e.target.value)}
-//           className="bg-white border"
-//           placeholder="0.0"
-//           min="0.00"
-//           step="0.01"
-//           max="100.00"
-//         />
-//         <InputGroup.Text className="bg-light fw-bold text-secondary small">
-//           %
-//         </InputGroup.Text>
-//       </InputGroup>
-//     </Form.Group>
-
-//     <Form.Control
-//       placeholder="Enter Invoice ID"
-//       name="copyId"
-//       value={copyId}
-//       onChange={(e) => setCopyId(e.target.value)}
-//       type="text"
-//       className="my-2 bg-white border"
-//     />
-//     <Button
-//       variant="primary"
-//       onClick={handleCopyInvoice}
-//       className="d-block"
-//     >
-//       Copy Old Invoice
-//     </Button>
-//   </div>
-// </Col>
-// </Row>
-// </Form>)})}
